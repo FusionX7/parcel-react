837fd22e74134a9494e563b14d52f5beb14189c6
FusionX7 <badang789@gmail.com>
2019-04-13 13:50:56 +0700

    test

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './index.css';
-import './style.scss'
-const App = (props) => {
-  const [test,setTest] = useState(0);
-  return (
-  <div onClick={()=>setTest(test+1)} className="App">
-    <img className="App-Logo" src={logo} alt="React Logo" />
-    <h1 className="App-Title">Hello Parcel x React - {test}</h1>
-  </div>
-)};
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import logo from "./logo.svg";
+import "./index.css";
+import "./style.scss";
+console.log("aaaa");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const App = props => {
+  const [test, setTest] = useState(0);
+  return (
+    <div onClick={() => setTest(test + 1)} className="App">
+      <img className="App-Logo" src={logo} alt="React Logo" />
+      <h1 className="App-Title">Hello Parcel x React - {test}</h1>
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // Hot Module Replacement
 if (module.hot) {
